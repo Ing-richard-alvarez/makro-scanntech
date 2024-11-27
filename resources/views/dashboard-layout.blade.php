845e5ed968e7304b74412945af04df48ec567053
Richard Alvarez Salazar <richardalvarez@periferia-it.com>
2024-11-27 17:55:24 -0500

    layout modified

--- resources/views/dashboard-layout.blade.php
+++ resources/views/dashboard-layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Panel de Control - Scanntech</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
